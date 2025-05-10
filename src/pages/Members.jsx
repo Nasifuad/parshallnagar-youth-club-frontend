1116673a97ae0919bc781bb9439e44c8515db4ee
Nasifuad <nasifuad007@gmail.com>
2025-05-10 23:38:12 +0600

    d

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -161,7 +161,7 @@ const MembersDashboard = () => {
         </div>
       </motion.div>
 
-      {/* সদস্য কার্ড গ্রিড */}
+      {/* সদস্য কার্ড গ্রিড % */}
       <motion.div
         className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
         initial="hidden"
